9db14011ad62eb80068882f817067546466537db
HenriqueCamacho1 <122056117+HenriqueCamacho1@users.noreply.github.com>
2023-03-23 01:40:04 +0000

    Update settings.py

--- LEPLATAFORME/settings.py
+++ LEPLATAFORME/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-srhnc34m)cf74bu0zmd3c7v*uniyxlr64ov62r6jrmyrn6yf@g'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
